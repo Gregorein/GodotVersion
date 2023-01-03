d44a8ecca9c5d680f282133f62c9caca8669d3a9
Greg Bak <gregbak@me.com>
2023-01-03 13:19:39 +0100

    feat: added a guard for missing files

--- plugin.gd
+++ plugin.gd
@@ -3,8 +3,11 @@ extends EditorPlugin
 
 func build() -> bool:
 	var file = File.new()
-	file.open("res://GitVersion.json", File.READ)
-	var version = parse_json(file.get_as_text())
-	print("version: ", version["SemVer"])
-	ProjectSettings.version = version
+	var error = file.open("res://GitVersion.json", File.READ)
+	if (error != OK):
+		var version = parse_json(file.get_as_text())
+		print("version: ", version["SemVer"])
+		ProjectSettings.version = version
+		file.close()
+
 	return true
