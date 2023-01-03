245309029758fb02ef6dd698c44a692fd73d346b
Greg Bak <gregbak@me.com>
2023-01-03 13:49:42 +0100

    fix: fixed guard excluding correct state

--- plugin.gd
+++ plugin.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 func build() -> bool:
 	var file = File.new()
-	var error = file.open("res://GitVersion.json", File.READ)
-	if (error != OK):
+	var fileStatus = file.open("res://GitVersion.json", File.READ)
+	if (fileStatus == OK):
 		var version = parse_json(file.get_as_text())
 		print("version: ", version["SemVer"])
 		ProjectSettings.version = version
